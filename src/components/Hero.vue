ca550272276ac10c456f415edf3d60fb87834d65
Cleutjens <luc.cleutjens@gmail.com>
2019-12-27 13:59:31 +0100

    Update Hero.vue

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -56,6 +56,12 @@ export default {};
   }
 }
 
+@media (min-width: $screen-md-min) {
+  .hero-content {
+    margin-bottom: 60px;
+  }
+}
+
 @media (max-width: $screen-md-min) {
   .hero-content {
     margin-bottom: 160px;
