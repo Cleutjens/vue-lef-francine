65b084fae37c44009bc2402e8fde8f28fe203003
Cleutjens <luc.cleutjens@gmail.com>
2019-12-27 15:01:33 +0100

    bg position fix

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -17,17 +17,6 @@
 </template>
 
 <script>
-// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-let vh = window.innerHeight * 0.01;
-// Then we set the value in the --vh custom property to the root of the document
-document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-// We listen to the resize event
-window.addEventListener("resize", () => {
-  // We execute the same script as before
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty("--vh", `${vh}px`);
-});
 </script>
 
 <style lang="scss" scoped>
@@ -53,10 +42,10 @@ window.addEventListener("resize", () => {
   background-attachment: fixed;
 }
 
-@media only screen and (min-width: 768px) {
+@media only screen and (max-width: 768px) {
   .hero {
-    // height: 100vh;
-    background-attachment: none;
+    // height: 80vh;
+    background-attachment: scroll;
   }
 }
 
