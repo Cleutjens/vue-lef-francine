32474db4bb4176c233f6bcadfef7a48bedc567ea
Cleutjens <luc.cleutjens@gmail.com>
2020-01-12 14:15:35 +0100

    how it works added / changed

--- src/components/Howitworks.vue
+++ src/components/Howitworks.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="wrapper">
+  <!-- <div class="wrapper">
     <div class="container">
       <h2>Hoe het werkt</h2>
     </div>
@@ -34,12 +34,89 @@
         </div>
       </div>
     </div>
+  </div>-->
+  <div class="howitworks">
+    <div class="container-fluid">
+      <div class="container">
+        <div class="col-xs">
+          <h2>Hoe het werkt</h2>
+        </div>
+      </div>
+    </div>
+    <div class="swipe-wrapper">
+      <div class="swiper-container">
+        <swiper :options="swiperOptions2">
+          <swiper-slide>
+            <div class="timeline-block timeline-step1">
+              <div class="handshake">
+                <img src="../assets/handshake-icon.svg" alt />
+                <p class="number number-first">1</p>
+                <hr />
+                <p class="subtitle">Persoonlijk intake gesprek</p>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="timeline-block timeline-step2">
+              <div class="brief">
+                <img src="../assets/brief-icon.svg" alt />
+                <p class="number number-first">2</p>
+                <hr />
+                <p class="subtitle">Debrief opdracht en offerte</p>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="timeline-block timeline-step2">
+              <div class="unlock">
+                <img src="../assets/unlock-icon.svg" alt />
+                <p class="number number-first">3</p>
+                <hr />
+                <p class="subtitle">Ontwerp maatwerk programma</p>
+              </div>
+            </div>
+          </swiper-slide>
+          <swiper-slide>
+            <div class="timeline-block timeline-step4">
+              <div class="rocket">
+                <img src="../assets/rocket-icon.svg" alt />
+                <p class="number number-first">4</p>
+                <hr />
+                <p class="subtitle">Start versnelling</p>
+              </div>
+            </div>
+          </swiper-slide>
+        </swiper>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import "swiper/dist/css/swiper.css";
+import { swiper, swiperSlide } from "vue-awesome-swiper";
+
 export default {
-  name: "Howitworks"
+  name: "Howitworks",
+  components: {
+    swiper,
+    swiperSlide
+  },
+  data() {
+    return {
+      swiperOptions2: {
+        slidesPerView: "4",
+        spaceBetween: 0,
+        noSwiping: true,
+        noSwipingClass: "swiper-container2",
+        breakpoints: {
+          720: {
+            slidesPerView: "1"
+          }
+        }
+      }
+    };
+  }
 };
 </script>
 
@@ -51,45 +128,52 @@ export default {
   padding: 0px 80px;
   margin-top: 130px;
   margin-bottom: 65px;
+  text-align: center;
 }
 
-.timeline {
-  padding: 0px 80px;
-  margin-bottom: 140px;
+@media (max-width: $screen-md-min) {
+  .container {
+    padding: 0px 64px;
+    text-align: left;
+  }
+}
+
+@media (max-width: $screen-sm-min) {
+  .container {
+    padding: 0px 32px;
+  }
+}
+
+.swipe-wrapper {
+  padding: 0 80px;
+  margin-bottom: 167px;
 }
 
 @media (max-width: $screen-lg-min) {
-  .container {
-    padding: 0 32px;
+  .swipe-wrapper {
+    padding: 0 64px;
   }
-  .timeline {
+}
+
+@media (max-width: $screen-sm-min) {
+  .swipe-wrapper {
     padding: 0 32px;
   }
 }
 
-ul.timeline-ul {
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  width: 100%;
-  img {
-    width: 60px;
-    height: 60px;
+@media (max-width: $screen-md-min) {
+  .swipe-wrapper {
+    padding: 0;
   }
 }
-ul {
-  display: flex;
-  flex-direction: column;
-  flex-basis: 100%;
-  flex: 1;
-  padding: 0;
+
+.timeline-block {
+  padding-top: 80px;
+  padding-bottom: 80px;
 }
-ul li {
-  text-decoration: none;
-  list-style: none;
-}
-li {
-  flex: 3;
+
+.swiper-wrapper {
+  width: 100%;
 }
 
 p.number {
@@ -100,11 +184,22 @@ p.number {
   font-size: 12px;
   padding-top: 4px;
   margin-top: 48px;
+  margin-bottom: 48px;
   background: #bf0000;
   color: $white;
   width: 24px;
-  height: 24px;
+  height: 20px;
   z-index: 100;
+}
+
+hr {
+  display: block;
+  margin-left: auto;
+  margin-top: -60px;
+  border: 0;
+  border-bottom: 1px dashed #fff;
+  background: #b4b4b4;
+  z-index: -1;
 }
 
 p.subtitle {
@@ -112,67 +207,261 @@ p.subtitle {
   font-weight: 900;
   color: $black;
   text-transform: uppercase;
-  display: block;
   max-width: 160px;
   text-align: left;
-  margin-top: 28px;
+  margin-top: 32px;
+}
+
+.timeline-step2 {
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+
+  p.number {
+    margin: 48px auto;
+  }
+
+  p.subtitle {
+    margin: 48px auto 0 auto;
+    text-align: center;
+  }
+
+  img {
+    margin: 0 auto;
+  }
 }
 
 @media (max-width: $screen-md-min) {
-  p.subtitle {
-    font-size: 12px;
-    max-width: 80px;
-  }
-  ul {
-    display: inline-block;
-  }
-}
-
-hr {
-  display: block;
-  margin-left: auto;
-  margin-top: -84px;
-  border: 0;
-  border-bottom: 1px dashed #fff;
-  background: #b4b4b4;
-  z-index: -1;
-}
-
-li.brief {
-  hr {
-    margin-top: -106px;
-  }
-}
-
-li.unlock {
-  hr {
-    margin-top: -106px;
-  }
-}
-
-@media (max-width: 720px) {
-  hr {
-    margin-top: -84px;
-  }
-  li.unlock {
-    hr {
-      margin-top: -84px;
+  .timeline-step2 {
+    margin-left: 64px;
+    p.subtitle {
+      margin: 48px 0 0 0;
+      text-align: left;
     }
-  }
-  li.brief {
-    hr {
-      margin-top: -84px;
+    p.number {
+      margin: 48px 0;
     }
-  }
-  p.subtitle {
-    font-size: 10px;
-  }
-  ul.timeline-ul {
+    hr {
+      width: 100%;
+      margin-left: 0;
+    }
     img {
-      width: 48px;
-      height: 48px;
+      margin-right: auto;
+      margin-left: 0;
     }
   }
 }
+
+@media (max-width: $screen-sm-min) {
+  .timeline-step2 {
+    margin-left: 32px;
+  }
+}
+
+.timeline-step1 {
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+
+  p.number {
+    margin: 48px auto;
+  }
+
+  p.subtitle {
+    margin: 48px auto 0 auto;
+    text-align: center;
+  }
+
+  img {
+    margin: 0 auto;
+  }
+  hr {
+    width: 50%;
+    margin-right: 0;
+  }
+}
+
+@media (max-width: $screen-md-min) {
+  .timeline-step1 {
+    margin-left: 64px;
+    p.subtitle {
+      margin: 48px 0 0 0;
+      text-align: left;
+    }
+    p.number {
+      margin: 48px 0;
+    }
+    hr {
+      width: 100%;
+      margin-left: 0;
+    }
+    img {
+      margin-right: auto;
+      margin-left: 0;
+    }
+  }
+
+  .timeline-block {
+    cursor: move; /* fallback if grab cursor is unsupported */
+    cursor: grab;
+    cursor: -moz-grab;
+    cursor: -webkit-grab;
+
+    -webkit-touch-callout: none;
+    -webkit-user-select: none;
+    -khtml-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+
+    &:active {
+      cursor: grabbing;
+      cursor: -moz-grabbing;
+      cursor: -webkit-grabbing;
+    }
+  }
+}
+
+@media (max-width: $screen-sm-min) {
+  .timeline-step1 {
+    margin-left: 32px;
+  }
+}
+
+.timeline-step4 {
+  width: 100%;
+  text-align: center;
+  margin: 0 auto;
+
+  p.number {
+    margin: 48px auto;
+  }
+
+  p.subtitle {
+    margin: 48px auto 0 auto;
+    text-align: center;
+  }
+
+  img {
+    margin: 0 auto;
+  }
+
+  hr {
+    width: 50%;
+    margin-left: 0;
+  }
+}
+
+@media (max-width: $screen-md-min) {
+  .timeline-step4 {
+    margin-left: 64px;
+    p.subtitle {
+      margin: 48px 0 0 0;
+      text-align: left;
+    }
+    p.number {
+      margin: 48px 0;
+    }
+    hr {
+      background: #fff;
+    }
+    img {
+      margin-right: auto;
+      margin-left: 0;
+    }
+  }
+}
+
+@media (max-width: $screen-sm-min) {
+  .timeline-step4 {
+    margin-left: 32px;
+  }
+}
+
+// .timeline {
+//   padding: 0px 80px;
+//   margin-bottom: 140px;
+// }
+
+// @media (max-width: $screen-lg-min) {
+//   .container {
+//     padding: 0 32px;
+//   }
+//   .timeline {
+//     padding: 0 32px;
+//   }
+// }
+
+// ul.timeline-ul {
+//   display: flex;
+//   flex-direction: row;
+//   flex-wrap: wrap;
+//   width: 100%;
+//   img {
+//     width: 60px;
+//     height: 60px;
+//   }
+// }
+// ul {
+//   display: flex;
+//   flex-direction: column;
+//   flex-basis: 100%;
+//   flex: 1;
+//   padding: 0;
+// }
+// ul li {
+//   text-decoration: none;
+//   list-style: none;
+// }
+// li {
+//   flex: 3;
+// }
+
+// @media (max-width: $screen-md-min) {
+//   p.subtitle {
+//     font-size: 12px;
+//     max-width: 80px;
+//   }
+//   ul {
+//     display: inline-block;
+//   }
+// }
+
+// li.brief {
+//   hr {
+//     margin-top: -106px;
+//   }
+// }
+
+// li.unlock {
+//   hr {
+//     margin-top: -106px;
+//   }
+// }
+
+// @media (max-width: 720px) {
+//   hr {
+//     margin-top: -84px;
+//   }
+//   li.unlock {
+//     hr {
+//       margin-top: -84px;
+//     }
+//   }
+//   li.brief {
+//     hr {
+//       margin-top: -84px;
+//     }
+//   }
+//   p.subtitle {
+//     font-size: 10px;
+//   }
+//   ul.timeline-ul {
+//     img {
+//       width: 48px;
+//       height: 48px;
+//     }
+//   }
+// }
 </style>
 
